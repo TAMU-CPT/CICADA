cbe18085126be3834e48d8132ed7bf42502991e5
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-04 20:30:54 -0500

    creates new directories and files

--- {{cookiecutter.dir_name}}/script.py
+++ {{cookiecutter.dir_name}}/script.py
@@ -3,8 +3,16 @@ import ast
 import _ast
 
 path = '{{cookiecutter.models_path}}'
-models = []
 app_name = '{{cookiecutter.app_name}}'
+models = []
+
+def template_ctrl(name):
+    """ templates the contents of the controller files """
+    return '\n'.join(["export default function(%s) {" % app_name,
+                " "*4 + "%s" % app_name + ".controller('%s', ['$scope','$location'," % name,
+                " "*8 + "function($scope, $location) {",
+                " "*4 + "}]);",
+                "}"])
 
 # parse models.py
 with open(os.path.expanduser(path)) as models_file:
@@ -13,6 +21,7 @@ with open(os.path.expanduser(path)) as models_file:
         if type(i) == _ast.ClassDef:
             models.append(i.name)
 
+# modify app.js and create controllers and templates for list/detail views
 with open('js/app.js', 'r+') as app:
     a = app.read()
     routes = []
@@ -35,9 +44,21 @@ with open('js/app.js', 'r+') as app:
                    " "*16 + "controller: '%sDetailCtrl'" % model,
                    " "*12 + "})."]))
 
-        os.makedirs('./ctrl/%s' % model.lower())
         requires.append("require('./ctrl/%s/list.js')" % model.lower() + "(%s);" % app_name)
         requires.append("require('./ctrl/%s/detail.js')" % model.lower() + "(%s);" % app_name)
+
+        # create directories for each model's set of controls
+        os.makedirs('js/ctrl/%s' % model.lower())
+
+        # create list and detail controllers for each model
+        with open('js/ctrl/%s/list.js' % model, 'w') as list_ctrl:
+            list_ctrl.write(template_ctrl(model + "ListCtrl"))
+        with open('js/ctrl/%s/detail.js' % model, 'w') as detail_ctrl:
+            detail_ctrl.write(template_ctrl(model + "DetailCtrl"))
+
+        # create list and detail templates for each model
+        open('partials/%s-list.html' % model.lower(), 'a').close()
+        open('partials/%s-detail.html' % model.lower(), 'a').close()
 
     a = a.replace('// LOAD ROUTES', '\n'.join(routes))
     a = a.replace('// REQUIRE', '\n'.join(requires))
